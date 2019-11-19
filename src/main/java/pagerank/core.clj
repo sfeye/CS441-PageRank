d26c631a0ed5f6ca8bd6b8a4c608975c48e6fc46
Sam Feye <samfeye@gmail.com>
2019-11-19 16:32:16 -0600

    Added comments showing where I'm making mistakes

--- src/main/java/pagerank/core.clj
+++ src/main/java/pagerank/core.clj
@@ -1,14 +1,18 @@
 (ns pagerank.core (:require clojure.pprint))
 
+;opens file and returns a sequence of the file by line
 (def seq-file-lines (with-open [rdr (clojure.java.io/reader (clojure.java.io/resource "pages.txt"))]
                       (reduce conj [] (line-seq rdr))))
 
+;this returns 10,000 little maps
 (def link-map (for [line seq-file-lines]
                 (let [nums (clojure.string/split line #" ")
                       id (read-string (first nums))
                       links (rest nums)]
                   (hash-map id links))))
 
+;this is wrong, it currently makes 10,000 little maps
+;i want to merge these into one global map, but maps are immutable
 (defn MakeMap []
   (for [line seq-file-lines]
     (let [nums (clojure.string/split line #" ")
@@ -18,7 +22,9 @@
       (hash-map (keyword id) rank))
     ))
 
+;using this defn to test my merging/assoc attempts
 (defn PrintTest []
+  ;currently this makes a one element seq of 10,000 maps and prints it
   (let [big-map (MakeMap)]
   (clojure.pprint/pprint big-map)))
 
