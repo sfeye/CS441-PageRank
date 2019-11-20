2736d78195b5fbf3755ab591f7f32fbc3fd8ae4b
Sam Feye <samfeye@gmail.com>
2019-11-20 15:03:38 -0600

    Ha! F off clojure, super gross, but it actually works!

--- src/main/java/pagerank/core.clj
+++ src/main/java/pagerank/core.clj
@@ -4,19 +4,21 @@
   (with-open [rdr (clojure.java.io/reader (clojure.java.io/resource file))]
     (doall (line-seq rdr))))
 
-(defn split-file []
+(defn rank-map []
   (for [line (read-lines "pages.txt")]
     (let [nums (clojure.string/split line #" ")
           id (first nums)
           links (rest nums)
           rank (count links)]
-      (hash-map (keyword id) rank))
-    ))
+      (hash-map (keyword id) rank)))
+  )
 
-(defn make-map [line-seq]
+(defn link-map [line-seq]
   (reduce conj (map hash-map [:id :links] (.split line-seq " " 2))))
 
 (defn PrintTest []
-  (clojure.pprint/pprint (nth (map make-map (read-lines "pages.txt")) 9999)))
+  (clojure.pprint/pprint (nth (map link-map (read-lines "pages.txt")) 9999))
+  (clojure.pprint/pprint (nth (rank-map) 9999))
+  )
 
 (PrintTest)
