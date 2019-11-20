21a1a19d9d440ed5c1e13c3838aeecdac15b1584
Sam Feye <samfeye@gmail.com>
2019-11-20 15:24:49 -0600

    Boom there's some fixes and assurances

--- src/main/java/pagerank/core.clj
+++ src/main/java/pagerank/core.clj
@@ -4,21 +4,30 @@
   (with-open [rdr (clojure.java.io/reader (clojure.java.io/resource file))]
     (doall (line-seq rdr))))
 
-(defn rank-map []
-  (for [line (read-lines "pages.txt")]
+(defn rank-map [line-seq]
+  (for [line line-seq]
     (let [nums (clojure.string/split line #" ")
-          id (first nums)
+          id (read-string (first nums))
           links (rest nums)
           rank (count links)]
-      (hash-map (keyword id) rank)))
+      (hash-map :id id, :rank rank)))
   )
 
 (defn link-map [line-seq]
   (reduce conj (map hash-map [:id :links] (.split line-seq " " 2))))
 
+(def link-coll (map link-map (read-lines "pages.txt")))
+(def rank-coll (rank-map (read-lines "pages.txt")))
+
+
+;for testing
 (defn PrintTest []
-  (clojure.pprint/pprint (nth (map link-map (read-lines "pages.txt")) 9999))
-  (clojure.pprint/pprint (nth (rank-map) 9999))
+  (clojure.pprint/pprint (nth link-coll 9999))
+  (clojure.pprint/pprint (get (nth link-coll 9999) :id))
+  (clojure.pprint/pprint (get (nth link-coll 9999) :links))
+  (clojure.pprint/pprint (nth rank-coll 9999))
+  (clojure.pprint/pprint (get (nth rank-coll 9999) :id))
+  (clojure.pprint/pprint (get (nth rank-coll 9999) :rank))
   )
 
 (PrintTest)
