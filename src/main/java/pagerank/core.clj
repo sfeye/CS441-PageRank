6647c795e7b67cab56ebf40dc54bebeeb4d74461
Sam Feye <samfeye@gmail.com>
2019-11-20 15:39:33 -0600

    More testing, changing application to remove links strings

--- src/main/java/pagerank/core.clj
+++ src/main/java/pagerank/core.clj
@@ -7,24 +7,32 @@
 (defn rank-map [line-seq]
   (for [line line-seq]
     (let [nums (clojure.string/split line #" ")
-          id (read-string (first nums))
+          id (first nums)
           links (rest nums)
           rank (count links)]
       (hash-map :id id, :rank rank)))
   )
 
 (defn link-map [line-seq]
-  (reduce conj (map hash-map [:id :links] (.split line-seq " " 2))))
+  (for [line line-seq]
+    (let [nums (clojure.string/split line #" ")
+          id (first nums)
+          links (rest nums)]
+      (hash-map :id id, :links links)))
+  )
 
-(def link-coll (map link-map (read-lines "pages.txt")))
+(def link-coll (link-map (read-lines "pages.txt")))
 (def rank-coll (rank-map (read-lines "pages.txt")))
 
+(defn compute-rank [id]
+  )
 
 ;for testing
 (defn PrintTest []
   (clojure.pprint/pprint (nth link-coll 9999))
   (clojure.pprint/pprint (get (nth link-coll 9999) :id))
   (clojure.pprint/pprint (get (nth link-coll 9999) :links))
+  (println "5th:" (nth (get (nth link-coll 9999) :links) 5))
   (clojure.pprint/pprint (nth rank-coll 9999))
   (clojure.pprint/pprint (get (nth rank-coll 9999) :id))
   (clojure.pprint/pprint (get (nth rank-coll 9999) :rank))
