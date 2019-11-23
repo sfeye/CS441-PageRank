bd4f61ff43118bb2d2745aaa7e35d362665e967b
Sam Feye <samfeye@gmail.com>
2019-11-23 12:34:19 -0600

    So guess work

--- src/main/java/pagerank/core.clj
+++ src/main/java/pagerank/core.clj
@@ -1,6 +1,8 @@
 (ns pagerank.core (:require clojure.pprint))
 
 (def damping-factor 0.85)
+(def one-minus-damp 0.15)
+;used to apply first step 1/10,000
 (def initial-ranks 0.0001)
 
 (defn read-lines [file]
@@ -12,10 +14,8 @@
 (defn rank-map [line-seq]
   (for [line line-seq]
     (let [nums (clojure.string/split line #" ")
-          id (first nums)
-          links (rest nums)
-          rank (count links)]
-      (hash-map :id id, :rank rank)
+          id (first nums)]
+      (hash-map :id id, :rank initial-ranks)
       )
     )
   )
@@ -33,14 +33,21 @@
 (def link-coll (link-map (read-lines "pages.txt")))
 (def rank-coll (rank-map (read-lines "pages.txt")))
 
-(defn compute-rank []
-  (let [pr-map {}]
-    (loop [x 1000]
-      (when (> x 0)
-        ;compute the ranks here
-        (recur (- x 1))
-        )
-      )
+(defn sum-ranks [id]
+  (let [count-x (count (get (nth link-coll id) :links))
+        curr-rank-x (get (nth rank-coll id) :rank)
+        updated-rank (* damping-factor (/ curr-rank-x count-x))]
+    (println updated-rank))
+  )
+
+(defn assoc-rank []
+  (loop [x 1]
+    (when (> x 0)
+      (loop [y 9999]
+        (when (> y 0)
+          (println y)
+          (recur (- y 1)))))
+    (recur (- x 1))
     )
   )
 
@@ -55,4 +62,4 @@
   (clojure.pprint/pprint (get (nth rank-coll 9999) :rank))
   )
 
-(compute-rank)
+(sum-ranks 0)
