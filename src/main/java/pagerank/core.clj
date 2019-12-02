8cbbb10ca583463f8dcf279aa711c55c9267a389
Sam Feye <samfeye@gmail.com>
2019-12-02 14:09:00 -0600

    Added file IO outputs and shit ton of other stuff

--- src/main/java/pagerank/core.clj
+++ src/main/java/pagerank/core.clj
@@ -1,21 +1,21 @@
-(ns pagerank.core (:require clojure.pprint))
+(ns pagerank.core (:require [clojure.pprint]))
 
 (def damping-factor 0.85)
 (def one-minus-damp 0.15)
 (def initial-ranks 1)
 
-(defn read-lines [file]
-  (with-open [rdr (clojure.java.io/reader (clojure.java.io/resource file))]
-    (doall (line-seq rdr))))
+(defn read-file [file]
+  (with-open [reader (clojure.java.io/reader (clojure.java.io/resource file))]
+    (doall (line-seq reader))))
 
-(defn link-map [line-seq]
-  (for [line line-seq]
+(defn link-map [lines]
+  (for [line lines]
     (let [nums (clojure.string/split line #" ")
           id (first nums)
           links (rest nums)]
       (hash-map :id id, :links links))))
 
-(def link-coll (link-map (read-lines "pages.txt")))
+(def link-coll (link-map (read-file "pages.txt")))
 (def no-loop (atom (set (range 0 10000))))
 (def rank-coll (atom (zipmap (range 0 10000) (repeat initial-ranks))))
 
@@ -29,44 +29,53 @@
   (for [links (get (nth link-coll id) :links)]
     (update-rank-comp (read-string links))))
 
+;this is the method that is taking too long, reduce is the potential problem
 (defn sum-ranks-comp [id]
-  (reduce + (line-ranks-comp id)))
+  (reduce + (doall (line-ranks-comp id))))
 
 (defn apply-damp-comp [id]
   (+ one-minus-damp (* damping-factor (sum-ranks-comp id))))
 
 (defn abs [num] (max num (- num)))
 
-(defn difference [x]
-  (let [y (- (apply-damp-comp x) (get @rank-coll x))]
-    (abs y) )
-  )
-
 (defn rank-step []
-  (doseq [x @no-loop]
-        (if (< (difference x) 0.00001) (swap! no-loop disj x) (swap! rank-coll assoc x (apply-damp-comp x)))))
+    (doseq [x @no-loop]
+      (let [damp-comp (apply-damp-comp x)
+            curr-rank (get @rank-coll x)]
+        (if (< (abs (- damp-comp curr-rank)) 0.01) (swap! no-loop disj x) (swap! rank-coll assoc x damp-comp)))))
 
 (defn number-of-iterations [y]
-  (dotimes [_ (Math/round (double y))]
-    (rank-step)))
+  (dotimes [_ y]
+    (when (> (count @no-loop) 0) (rank-step))))
 
 (defn number-of-threads [t]
-  (let [num-it (/ 100 t)
+  (let [num-it (Math/round (double (/ 1000 t)))
         fs (for [_ (range t)]
              (future (number-of-iterations num-it)))]
     (doseq [f fs]
       @f)))
 
+(defn to-file [time]
+  (spit (clojure.java.io/resource "outputs.txt") time))
+
+(defn out-times-to-file []
+  (to-file (time (number-of-threads 1)))
+  (to-file (time (number-of-threads 2)))
+  (to-file (time (number-of-threads 4)))
+  (to-file (time (number-of-threads 8)))
+  (to-file (time (number-of-threads 16)))
+  (to-file (time (number-of-threads 32)))
+  (to-file (time (number-of-threads 64)))
+  )
 
 ;for testing
 (defn print-test []
-  ;(clojure.pprint/pprint @no-loop)
-  (clojure.pprint/pprint @rank-coll)
+  ;(to-file "test")
+  (clojure.pprint/pprint (count @no-loop))
+  ;(clojure.pprint/pprint @rank-coll)
   )
 
-;(time (number-of-iterations 100))
-(time (number-of-threads 64))
+;(time (number-of-iterations 1000))
+(time (number-of-threads 8))
 ;(time (rank-step))
-(print-test)
-
-
+;(print-test)
